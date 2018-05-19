323f0f87f1b6a93f7156a617d0c484c2bd2f8619
patatemz <31726128+patatemz@users.noreply.github.com>
2018-05-19 19:27:53 +0300

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,147 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+	  <title>REBC03 Artjoms Ivanovs Datromacība</title>
+<style type="text/css">
+   A { font-size: 14px; color: white; }
+ </style>
+<style>
+	footer {
+		background-color: white;
+		background-position: 100% 100%;
+		background-repeat: repeat;
+	}
+	body {
+		background-color: #285955;
+		background-position: 100% 100%;
+		background-repeat: repeat;
+	}
+	h2 {
+		color: white;
+		font-family: verdana;
+		font-size: 150%;
+	}
+	div {
+		text-align: center
+	}
+	h1 {
+		color: #285955;
+		font-family: verdana;
+		font-size: 300%;
+	}
+	h4 {
+		color: #FFFFFF;
+		font-family: verdana;
+		font-size: 300%;
+	}
+	p  {
+		color: white;
+		font-family: courier;
+		font-size: 160%;
+		text-align: center;
+	}
+	p1  {
+		color: #285955;
+		font-family: bold;
+		font-size: 100%
+	
+	}
+p.p1  {
+		color: #285955;
+		font-family: bold;
+		font-size: 18pt;
+		margin-top: 20px;
+		margin-bottom: 20px;
+		margin-right: 300px;
+		margin-left:250px;
+	
+}
+	h3  {
+		color: #285955;
+		font-family: verdana;
+		font-size: 150%;
+	}
+	ul { 
+    display: block;
+    list-style-type: disk;
+    margin-top: 1em;
+    margin-bottom: 1 em;
+    margin-left: 0;
+    margin-right: 0;
+    padding-left: 750px;
+}
+ol {
+    display: block;
+    list-style-type: decimal;
+    margin-top: 1em;
+    margin-bottom: 1em;
+    margin-left: 0;
+    margin-right: 0;
+    padding-left: 750px;
+}
+li {
+    display: list-item;
+	padding-left: 5px;
+	color: white
+}
+li1 {
+	list-style-type: circle;
+    display: list-item;
+	padding-left: 5px;
+	color: white
+}
+li2 {
+	list-style-type: circle;
+    display: list-item;
+	padding-left: 5px;
+	color: white;
+}
+table, th, td {
+    border: 1px solid White;
+	color: #FFFFFF;
+	text-align: center;
+	border-collapse: collapse
+
+}
+c {
+		color: white;
+		font-family: courier;
+		font-size: 100%;
+		text-align: center;
+}
+A {
+	   font-size: 14px; 
+	   color: white; 
+}
+A.link1 {
+	font-size: 14px; 
+	color: #285955;
+}
+</style>
+  </head>
+
+
+<footer>
+ <hr>
+    <div>
+      <h1><img src="https://www.rtu.lv/images/logo_lv.svg"><br>Datormacība 
+	  <span>
+	  <br>
+Darbu apkopojums
+	  </span>
+	  </h1>
+    </div>
+ <hr>
+</footer>
+    <body>
+<div>
+    <h4 id="S0">Saturs</h4>
+	</div>
+
+	<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+      <div>
+              <p1> &copy Rīgas Tehniskā universitāte 2018</p1>
+      </div>
+    </footer>
+
+</html>
